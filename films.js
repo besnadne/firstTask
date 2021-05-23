b39fe9ce39c3e5b71ca2343b89c9e07f2f4ec5f6
besnadne <serpov.box@gmail.com>
2021-05-23 10:02:43 +0300

    make questions for user

--- films.js
+++ films.js
@@ -1,3 +1,20 @@
 "use strict";
 
 let numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+
+
+let personalMovieDB = {
+    count: console.log(numberOfFilms),
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+let questionOne = prompt('Один из последних просмотреных фильмов?', '');
+let questionTwo = prompt('На сколько оцените его?', '');
+let questionThree = prompt('Один из последних просмотреных фильмов?', '');
+let questionFor = prompt('На сколько оцените его', '');
+personalMovieDB.movies = {
+    questionOne: questionTwo,
+    questionThree: questionFor
+};
