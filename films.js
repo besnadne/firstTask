2605e162e3aa9283d655f53471f3a8909e25933c
besnadne <serpov.box@gmail.com>
2021-05-24 15:25:19 +0300

    second task is done

--- films.js
+++ films.js
@@ -1,35 +1,38 @@
 "use strict";
 
-// const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+ const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
 
 
-//  const personalMovieDB = {
-//    count: numberOfFilms,
-//    movies: {},
-//    actors: {},
-//    genres: [],
-//    privat: false
-//  };
-//  if (personalMovieDB.count < 10) {
-//   console.log('Просмотрено довольно мало фильмов!');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
-//   console.log('Вы класический зрител');
-// } else {
-//   console.log('Вы киноман!');
-// }
-// const questionOne = prompt('Один из последних просмотреных фильмов?', ''),
-//   questionTwo = prompt('На сколько оцените его?', ''),
-//   questionThree = prompt('Один из последних просмотреных фильмов?', ''),
-//   questionFor = prompt('На сколько оцените его', '');
-// personalMovieDB.movies[questionOne] = questionTwo;
-// personalMovieDB.movies[questionThree] = questionFor;
-// console.log(personalMovieDB.movies);
-    let   questionOne,
-          questionTwo;
+ const personalMovieDB = {
+   count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+  };
+  if (personalMovieDB.count < 10) {
+   alert('Просмотрено довольно мало фильмов!');
+ } else if  (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+   alert('Вы класический зрител');
+ } else if (personalMovieDB.count >= 30) {
+   alert('Вы киноман!');
+  } else {
+    alert('Произошла ошибка!');
+  }
+ 
+let  questionOne,
+     questionTwo;
 
 for (let i = 0;i < 2;i++) {
     questionOne = prompt('Один из последних просмотреных фильмов?', '');
     questionTwo = prompt('На сколько оцените его?', '');
+    if (questionOne != '' && questionOne != null && questionOne.length < 50 && questionTwo != '' && questionTwo != null && questionTwo.length < 50) {
+      console.log('Done');
+      personalMovieDB.movies[questionOne] = questionTwo;
+    } else {
+      console.log('Error')
+      i--;
+    }
   }
-
+ console.log(personalMovieDB.movies);
  
