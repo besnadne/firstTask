97b7c46ab0955557da583e62f42b7aa48a8a34bc
besnadne <serpov.box@gmail.com>
2021-05-24 14:51:52 +0300

    create loop

--- films.js
+++ films.js
@@ -1,19 +1,35 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+// const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const questionOne = prompt('Один из последних просмотреных фильмов?', ''),
-  questionTwo = prompt('На сколько оцените его?', ''),
-  questionThree = prompt('Один из последних просмотреных фильмов?', ''),
-  questionFor = prompt('На сколько оцените его', '');
-personalMovieDB.movies[questionOne] = questionTwo;
-personalMovieDB.movies[questionThree] = questionFor;
-console.log(personalMovieDB.movies);
+//  const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false
+//  };
+//  if (personalMovieDB.count < 10) {
+//   console.log('Просмотрено довольно мало фильмов!');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+//   console.log('Вы класический зрител');
+// } else {
+//   console.log('Вы киноман!');
+// }
+// const questionOne = prompt('Один из последних просмотреных фильмов?', ''),
+//   questionTwo = prompt('На сколько оцените его?', ''),
+//   questionThree = prompt('Один из последних просмотреных фильмов?', ''),
+//   questionFor = prompt('На сколько оцените его', '');
+// personalMovieDB.movies[questionOne] = questionTwo;
+// personalMovieDB.movies[questionThree] = questionFor;
+// console.log(personalMovieDB.movies);
+    let   questionOne,
+          questionTwo;
+
+for (let i = 0;i < 2;i++) {
+    questionOne = prompt('Один из последних просмотреных фильмов?', '');
+    questionTwo = prompt('На сколько оцените его?', '');
+  }
+
+ 
